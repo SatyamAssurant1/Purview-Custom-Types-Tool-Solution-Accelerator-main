5abd36d5e5cd14559bfdf60c9fd16787d883fde7
SatyamAssurant1 <112850321+SatyamAssurant1@users.noreply.github.com>
2022-09-05 13:33:05 +0530

    Purview

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import { createContext, useContext } from 'react';
+
+// Create new auth context
+export const AuthContext = createContext();
+
+// React hook to use auth context
+export function useAuth() {
+  return useContext(AuthContext);
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import React     from 'react';
+import ReactDOM  from 'react-dom';
+import App from './App/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
